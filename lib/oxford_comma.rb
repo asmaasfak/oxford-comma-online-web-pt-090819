5b0e112188fd3c9edd79fc2b9d2ce3154684124f
avid-enhancer-8592 <asma_asfak@yahoo.com>
2019-10-02 00:18:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
 
+  if array.length == 2
+  array.join (" and ")
+  
+  else if array.length > 2
+  array[-1].insert( 0, "and ")
+  array.join(", ")
+
+  else array.flatten
+  end
 end
